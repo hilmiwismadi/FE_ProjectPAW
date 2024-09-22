c94f05f699eaae8dcbd719e62c4f6bc8439520cd
aDJi2003 <adjidharmawan87@gmail.com>
2024-09-22 08:54:57 +0700

    feat(page): add search functionality for mahasiswa

--- src/app/mahasiswa/page.tsx
+++ src/app/mahasiswa/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { Mahasiswa, columns } from "./columns";
 import { DataTable } from "@/components/data-table";
+import { useState, useEffect, useMemo } from "react";
 
 async function getMahasiswa(): Promise<Mahasiswa[]> {
   return [
@@ -24,107 +27,42 @@ async function getMahasiswa(): Promise<Mahasiswa[]> {
       email: "Alexturner@gmail.com",
       password: "Balaclava",
     },
-    {
-      id: "4",
-      nama: "Jimmy Butler",
-      nim: 497522,
-      email: "Jimmyb23@gmail.com",
-      password: "heatculture",
-    },
-    {
-      id: "5",
-      nama: "Chris Hemsworth",
-      nim: 497530,
-      email: "chris.hems@gmail.com",
-      password: "GodofThunder",
-    },
-    {
-      id: "6",
-      nama: "Michael Jordan",
-      nim: 497540,
-      email: "mjordan23@gmail.com",
-      password: "BullsDynasty",
-    },
-    {
-      id: "7",
-      nama: "Elon Musk",
-      nim: 497550,
-      email: "elonm@gmail.com",
-      password: "spacexMars",
-    },
-    {
-      id: "8",
-      nama: "Taylor Swift",
-      nim: 497555,
-      email: "tswift@gmail.com",
-      password: "Lover1989",
-    },
-    {
-      id: "9",
-      nama: "Robert Downey",
-      nim: 497560,
-      email: "rdj@gmail.com",
-      password: "IronMan",
-    },
-    {
-      id: "10",
-      nama: "Scarlett Johansson",
-      nim: 497565,
-      email: "scarlettj@gmail.com",
-      password: "BlackWidow",
-    },
-    {
-      id: "11",
-      nama: "Lebron James",
-      nim: 497570,
-      email: "lebronjames@gmail.com",
-      password: "KingJames",
-    },
-    {
-      id: "12",
-      nama: "Kanye West",
-      nim: 497575,
-      email: "kanye@gmail.com",
-      password: "YeezyBoost",
-    },
-    {
-      id: "13",
-      nama: "Emma Watson",
-      nim: 497580,
-      email: "emmaw@gmail.com",
-      password: "HermioneMagic",
-    },
-    {
-      id: "14",
-      nama: "Tom Holland",
-      nim: 497585,
-      email: "tomholland@gmail.com",
-      password: "SpiderMan",
-    },
-    {
-      id: "15",
-      nama: "Natalie Portman",
-      nim: 497590,
-      email: "natalie@gmail.com",
-      password: "MightyThor",
-    },
-    {
-      id: "16",
-      nama: "Lionel Messi",
-      nim: 497595,
-      email: "lmessi10@gmail.com",
-      password: "GOAT",
-    },
   ];
 }
 
-export default async function Page() {
-  const data = await getMahasiswa();
+export default function Page() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [data, setData] = useState<Mahasiswa[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchData = await getMahasiswa();
+      setData(fetchData);
+    }
+    fetchData();
+  }, []);
+
+  const filteredData = useMemo(() => {
+    return data.filter((mahasiswa) => 
+      mahasiswa.nama.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      mahasiswa.nim.toString().includes(searchTerm) ||
+      mahasiswa.email.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  }, [searchTerm, data]);
 
   return (
-    <section className="mx-auto py-10 bg-white w-full aspect-[1920/1080] flex flex-col justify-center items-center">
+    <section className="mx-auto py-10 bg-white w-full aspect-[1920/1080] flex flex-col justify-center items-center gap-5">
+      <div className="w-[80vw] h-8">
+        <input 
+          type="text"
+          placeholder="Search ..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full h-full bg-slate-200 rounded cursor-pointer px-2 py-1 text-black"
+        />
+      </div>
       <div className="w-[80vw] text-black ">
-        <DataTable columns={columns} data={data} />
+        <DataTable columns={columns} data={filteredData} />
       </div>
     </section>
   );

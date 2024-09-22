86c3677caab2151b8235ac17ea52d9481d70e82f
aDJi2003 <adjidharmawan87@gmail.com>
2024-09-22 10:57:48 +0700

    feat(page): add search icon on search bar mahasiswa

--- src/app/mahasiswa/page.tsx
+++ src/app/mahasiswa/page.tsx
@@ -3,6 +3,7 @@
 import { Mahasiswa, columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 import { useState, useEffect, useMemo } from "react";
+import Image from "next/image";
 
 async function getMahasiswa(): Promise<Mahasiswa[]> {
   return [
@@ -52,14 +53,15 @@ export default function Page() {
 
   return (
     <section className="mx-auto py-10 bg-white w-full aspect-[1920/1080] flex flex-col justify-center items-center gap-5">
-      <div className="w-[80vw] h-8">
+      <div className="w-[80vw] h-8 relative flex justify-center items-center">
         <input 
           type="text"
-          placeholder="Search ..."
+          placeholder="Search Mahasiswa ..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           className="w-full h-full bg-slate-200 rounded cursor-pointer px-2 py-1 text-black"
         />
+        <Image src={"/searchIcon.png"} width={16} height={16} alt="search icon" className="absolute right-4 cursor-pointer" />
       </div>
       <div className="w-[80vw] text-black ">
         <DataTable columns={columns} data={filteredData} />
